7ba6d0e6a5f2bf457e03c79d9a60e7b867db596f
Felipe Andaur <f.andaur@duocuc.cl>
2024-07-07 21:07:29 -0400

    Se incorporan cambios en Class ModeloMoto:Cilindraje,Peso,Potencia,Torque,Motor. Se arreglan detalles de acordion en Motos

--- Backend/administrador/forms.py
+++ Backend/administrador/forms.py
@@ -8,7 +8,3 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = "__all__"
-
-    def clean_precio(self):
-        precio = self.cleaned_data["precio"]
-        return round(precio, 2)
--- Backend/administrador/models.py
+++ Backend/administrador/models.py
@@ -13,6 +13,11 @@ class TipoMoto(models.Model):
 class ModeloMoto(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
     tipo = models.ForeignKey(TipoMoto, on_delete=models.PROTECT)
+    cilindraje = models.CharField(max_length=20, default="N/A")
+    peso = models.CharField(max_length=20, default="N/A")
+    potencia_maxima = models.CharField(max_length=50, default="N/A")
+    torque_maximo = models.CharField(max_length=50, default="N/A")
+    tipo_motor = models.CharField(max_length=200, default="N/A")
 
     def __str__(self):
         return f"{self.nombre} ({self.tipo})"
